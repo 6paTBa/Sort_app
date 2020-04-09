f25fe3845193f3b3658d768420f7631820d21f76
Danya <danil.sitnikov2000@yandex.ru>
2020-04-09 13:23:28 +0700

    Add file opening

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
+#include <inttypes.h>
 #include <stdio.h>
-int main(){
-// hi L:>
-	return 0;
+
+int main(int argc, char* argv[])
+{
+    FILE* input;
+    if ((argc == 1) || (argc > 3)) {
+        printf("Wrong number of arguments\n");
+        return 0;
+    }
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+        printf("Unable to read data - file not found\n");
+        return 0;
+    }
+    fclose(input);
+    return 0;
 }
