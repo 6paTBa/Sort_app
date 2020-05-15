f1f323fbefa8bcbf1aafdb748394c2496158bcde
Danya <danil.sitnikov2000@yandex.ru>
2020-05-15 13:09:52 +0700

    Fix sort.h

--- sort.h
+++ sort.h
@@ -1,3 +1,5 @@
+void merge(double* arr_double, size_t low, size_t mid, size_t high);
+
 void mergesort(double* arr_double, size_t low, size_t high);
 
 void insertionsort(double* arr_double, size_t n);
