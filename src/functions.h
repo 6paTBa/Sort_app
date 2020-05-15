e9cbc7e0794ab0c1d82bf59d3aa036109e921ca5
Danya <danil.sitnikov2000@yandex.ru>
2020-05-15 23:08:05 +0700

    Fix functions.h

--- src/functions.h
+++ src/functions.h
@@ -9,5 +9,5 @@ int32_t check_string(char* str, size_t* is_uint);
 
 void sort_uint(FILE* input, FILE* output, size_t size);
 
-void sort_double(FILE* input,FILE* output, size_t size);
+void sort_double(FILE* input, FILE* output, size_t size);
 #endif
