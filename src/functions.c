d1f61a0989eda6d294ed30c981efb216de37848d
Danya <danil.sitnikov2000@yandex.ru>
2020-05-16 16:00:04 +0700

    Add check more cases

--- src/functions.c
+++ src/functions.c
@@ -13,16 +13,31 @@ size_t check_string(char* str, size_t* is_uint)
         return 0;
     }
     for (size_t i = 0; str[i] != '\n'; i++) {
-        if (i == 0 && str[i] == '-') {
+        if ((i == 0) && (str[i] == '-')) {
             *is_uint = 0;
-        } else if (i != 0 && str[i] == '.' && point == 0) {
-            if (str[i - 1] == '-') {
+        } else if ((i != 0) && (str[i] == '.') && (point == 0)) {
+            if ((str[i - 1] == '-') || (str[i + 1] == '\n')) {
                 return 0;
             }
             point = 1;
             *is_uint = 0;
         } else if (!isdigit(str[i])) {
             return 0;
+        }
+        if (str[i] == '0') {
+            if (i == 0) {
+                if ((str[i + 1] != '.') && (str[i + 1] != '\n')) {
+                    return 0;
+                }
+            } else if ((str[i - 1] == '-') && (str[i + 1] != '.')) {
+                return 0;
+            } else if (str[i + 1] == '.') {
+                for (size_t j = 2; str[i + j] == '0'; j++) {
+                    if (str[i + j + 1] == '\n') {
+                        return 0;
+                    }
+                }
+            }
         }
     }
     return 1;
