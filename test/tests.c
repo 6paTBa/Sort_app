2c7a21417602515584af07db92c2e243d6e45485
Danya <danil.sitnikov2000@yandex.ru>
2020-05-16 16:12:47 +0700

    Invalid string tests

--- test/tests.c
+++ test/tests.c
@@ -1,3 +1,128 @@
 #include "functions.h"
 #include <ctest.h>
 
+CTEST(check_invalid_string, invalid_minus)
+{
+    char string[] = "--1\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, invalid_minus_position)
+{
+    char string[] = "1-5\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, illegal_symbol)
+{
+    char string[] = "5b\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, illegal_symbol_2)
+{
+    char string[] = "b\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, illegal_symbol_3)
+{
+    char string[] = "1 . 5\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, illegal_symbol_4)
+{
+    char string[] = " 1.5\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, invalid_double)
+{
+    char string[] = ".5\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, invalid_double_2)
+{
+    char string[] = "5.\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, invalid_double_3)
+{
+    char string[] = "5.0.0\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, invalid_double_4)
+{
+    char string[] = "-.5\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, minus_zero)
+{
+    char string[] = "-0\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, minus_zero_2)
+{
+    char string[] = "-0.000000\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, empty_string)
+{
+    char string[] = "\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(check_invalid_string, invalid_int)
+{
+    char string[] = "01\n";
+    size_t is_uint = 1;
+    size_t expect = 0;
+    size_t result = check_string(string, &is_uint);
+    ASSERT_EQUAL(expect, result);
+}
